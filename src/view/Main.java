ac10ce95220a03bcf932718cffd80d86257baa20
Juan Sanin <juanpablosaninb@gmail.com>
2020-09-28 12:04:04 -0500

    Start screen added

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,9 +1,16 @@
 package view;
 
+import controller.Controller;
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet {
-
+	private Controller controller;
+	private int screen;
+	
+	private PImage background;
+	private PImage[] startScreen;
+	
 	public static void main(String[] args) {
 		PApplet.main(Main.class.getName());
 
@@ -11,14 +18,31 @@ public class Main extends PApplet {
 	
 	public void settings() {
 		size(800, 600);
+		
 	}
 
 	public void setup() {
+		controller=new Controller();
+		screen=1;
+		background=loadImage("images/Background.png");
 		
+		startScreen= new PImage[2];
+		startScreen[0]=loadImage("images/Start1.png");
+		startScreen[1]=loadImage("images/Start2.png");
 	}
 	
 	public void draw() {
+		switch(screen) {
+		case 1:
+			image(startScreen[0],0,0,800,600);
+			if (mouseX > 250 && mouseX < 550 && mouseY > 350 && mouseY < 415) {
+				image(startScreen[1],0,0,800,600);
+			}
+			break;
 		
+		}
+		fill(0);
+		text(mouseX+","+mouseY, mouseX, mouseY);
 	}
 	
 	public void mousePressed() {
